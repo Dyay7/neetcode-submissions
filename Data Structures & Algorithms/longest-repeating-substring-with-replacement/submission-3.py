449093c4ff794c50bfdd96ffd7625413ec2d4b70
Dyay7 <dyay6969@gmail.com>
2026-06-06 10:28:48 +0200

    Add: longest-repeating-substring-with-replacement - submission-3

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-3.py
@@ -0,0 +1,17 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        res = 0
+        charSet = set(s)
+
+        for c in charSet:
+            count = l = 0
+            for r in range(len(s)):
+                if s[r] == c:
+                    count += 1
+                # if the condition is not meet, we need to move the left pointer and discount from count if it matches the char
+                if (r - l + 1) - count > k: #a if or a while works here because we increase one at a time
+                    if s[l] == c:
+                        count -= 1
+                    l += 1
+                res = max(res, r - l + 1)
+        return res
