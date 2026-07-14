8a9dee71097745ec4107b9665d1105bb80d04fdb
Dyay7 <dyay6969@gmail.com>
2026-07-14 19:48:39 +0200

    Add: count-number-of-islands - submission-1

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-1.py
@@ -0,0 +1,55 @@
+from typing import List
+
+
+class DSU:
+    def __init__(self, n: int):
+        self.parent = list(range(n))
+        self.size = [1] * n
+
+    def find(self, x: int) -> int:
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+
+    def union(self, a: int, b: int) -> bool:
+        root_a = self.find(a)
+        root_b = self.find(b)
+
+        if root_a == root_b:
+            return False
+
+        if self.size[root_a] < self.size[root_b]:
+            root_a, root_b = root_b, root_a
+
+        self.parent[root_b] = root_a
+        self.size[root_a] += self.size[root_b]
+
+        return True
+
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        ROWS, COLS = len(grid), len(grid[0])
+        dsu = DSU(ROWS * COLS)
+
+        def index(r, c):
+            return r * COLS + c
+
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        islands = 0
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == '1':
+                    islands += 1
+                    for dr, dc in directions:
+                        nr, nc = r + dr, c + dc
+                        if (nr < 0 or nc < 0 or nr >= ROWS or
+                            nc >= COLS or grid[nr][nc] == "0"
+                        ):
+                            continue
+
+                        if dsu.union(index(r, c), index(nr, nc)):
+                            islands -= 1
+
+        return islands
